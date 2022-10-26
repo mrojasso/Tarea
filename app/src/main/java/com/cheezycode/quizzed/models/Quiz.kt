261e702cee1b9c9605ec8165051e339467db63e1
mrojasso <mrojas50105@ufide.ac.cr>
2022-10-25 21:06:53 -0600

    commit
    commit

--- app/src/main/java/com/cheezycode/quizzed/models/Quiz.kt
+++
@@ -1,7 +0,0 @@
-package com.cheezycode.quizzed.models
-
-data class Quiz (
-    var id : String = "",
-    var title: String = "",
-    var questions: MutableMap<String, Question> = mutableMapOf()
-)
