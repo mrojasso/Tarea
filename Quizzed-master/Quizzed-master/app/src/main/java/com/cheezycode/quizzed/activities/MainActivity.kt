abfc373f406f7886b4ce0f5b56b31d77dfe0a559
mrojasso <mrojas50105@ufide.ac.cr>
2022-10-25 21:03:53 -0600

    tarea
    tarea

---
+++ Quizzed-master/Quizzed-master/app/src/main/java/com/cheezycode/quizzed/activities/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.cheezycode.quizzed.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.cheezycode.quizzed.R
+import com.cheezycode.quizzed.adapters.QuizAdapter
+import com.cheezycode.quizzed.models.Quiz
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    lateinit var adapter: QuizAdapter
+    private var quizList = mutableListOf<Quiz>()
+    lateinit var firestore: FirebaseFirestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setUpViews()
+
+    }
+
+    private fun populateDummyData() {
+        quizList.add(Quiz("11-12-2021","11-12-2021"))
+        quizList.add(Quiz("13-10-2021","13-10-2021"))
+        quizList.add(Quiz("14-10-2021","14-10-2021"))
+        quizList.add(Quiz("15-10-2021","15-10-2021"))
+        quizList.add(Quiz("16-10-2021","16-10-2021"))
+        quizList.add(Quiz("17-10-2021","17-10-2021"))
+        quizList.add(Quiz("17-10-2021","17-10-2021"))
+        quizList.add(Quiz("17-10-2021","17-10-2021"))
+        quizList.add(Quiz("17-10-2021","17-10-2021"))
+        quizList.add(Quiz("17-10-2021","17-10-2021"))
+        quizList.add(Quiz("17-10-2021","17-10-2021"))
+        quizList.add(Quiz("17-10-2021","17-10-2021"))
+        quizList.add(Quiz("17-10-2021","17-10-2021"))
+        quizList.add(Quiz("17-10-2021","17-10-2021"))
+        quizList.add(Quiz("17-10-2021","17-10-2021"))
+
+    }
+
+    fun setUpViews() {
+        setUpFireStore()
+        setUpDrawerLayout()
+        setUpRecyclerView()
+        setUpDatePicker()
+    }
+
+    private fun setUpDatePicker() {
+        btnDatePicker.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
+            datePicker.show(supportFragmentManager, "DatePicker")
+            datePicker.addOnPositiveButtonClickListener {
+                Log.d("DATEPICKER", datePicker.headerText)
+                val dateFormatter = SimpleDateFormat("dd-mm-yyyy")
+                val date = dateFormatter.format(Date(it))
+                val intent = Intent(this, QuestionActivity::class.java)
+                intent.putExtra("DATE", date)
+                startActivity(intent)
+            }
+            datePicker.addOnNegativeButtonClickListener {
+                Log.d("DATEPICKER", datePicker.headerText)
+
+            }
+            datePicker.addOnCancelListener {
+                Log.d("DATEPICKER", "Date Picker Cancelled")
+            }
+        }
+    }
+
+    private fun setUpFireStore() {
+        firestore = FirebaseFirestore.getInstance()
+        val collectionReference = firestore.collection("quizzes")
+        collectionReference.addSnapshotListener { value, error ->
+            if(value == null || error != null){
+                Toast.makeText(this, "Error fetching data", Toast.LENGTH_SHORT).show()
+                return@addSnapshotListener
+            }
+            Log.d("DATA", value.toObjects(Quiz::class.java).toString())
+            quizList.clear()
+            quizList.addAll(value.toObjects(Quiz::class.java))
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        adapter = QuizAdapter(this, quizList)
+        quizRecyclerView.layoutManager = GridLayoutManager(this, 2)
+        quizRecyclerView.adapter = adapter
+    }
+
+    fun setUpDrawerLayout() {
+        setSupportActionBar(appBar)
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(this, mainDrawer, R.string.app_name, R.string.app_name)
+        actionBarDrawerToggle.syncState()
+        navigationView.setNavigationItemSelectedListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+            mainDrawer.closeDrawers()
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
